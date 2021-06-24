7335bd9a94b2c0ae66e2bcb20f4b439b811876a5
itp16402 <84923298+itp16402@users.noreply.github.com>
2021-06-24 15:32:06 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authPath: 'http://10.0.120.36:8080/sam-login-0.0.1-SNAPSHOT/', // authPath
-  menuPath: 'http://10.0.120.36:8080/sam-menu-0.0.1-SNAPSHOT/', // menuPath
-  mailPath: 'http://10.0.120.36:8080/sam-mail-0.0.1-SNAPSHOT/', // mailPath
+  authPath: 'http://10.1.218.36:8080/sam-login-0.0.1-SNAPSHOT/', // authPath
+  menuPath: 'http://10.1.218.36:8080/sam-menu-0.0.1-SNAPSHOT/', // menuPath
+  mailPath: 'http://10.1.218.36:8080/sam-mail-0.0.1-SNAPSHOT/', // mailPath
 };
