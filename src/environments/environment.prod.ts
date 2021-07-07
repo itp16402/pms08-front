eba89c6e059561b3d0e414ccb59798b8bf002326
itp16402 <84923298+itp16402@users.noreply.github.com>
2021-07-07 10:41:23 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authPath: 'https://huapms08.cloudns.cl/sam-login-0.0.1-SNAPSHOT/', // authPath
-  menuPath: 'https://huapms08.cloudns.cl/sam-menu-0.0.1-SNAPSHOT/', // menuPath
-  mailPath: 'https://huapms08.cloudns.cl/sam-mail-0.0.1-SNAPSHOT/', // mailPath
+  authPath: 'https://52.233.224.97/sam-login-0.0.1-SNAPSHOT/', // authPath
+  menuPath: 'https://52.233.224.97/sam-menu-0.0.1-SNAPSHOT/', // menuPath
+  mailPath: 'https://52.233.224.97/sam-mail-0.0.1-SNAPSHOT/', // mailPath
 };
